0407f99bdb5f6ab35808411c443c5fb5c3aceff9
Sebastian Gräßl <mail@bastilian.me>
2025-03-19 23:00:21 +0100

    chore(deps): Update fresh aaaaahhhhhaaaa!

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -8,7 +8,7 @@ import * as $_middleware from "./routes/_middleware.ts";
 import * as $api_image_fileName_ from "./routes/api/image/[fileName].ts";
 import * as $index from "./routes/index.tsx";
 import * as $YouTubeVideo from "./islands/YouTubeVideo.tsx";
-import { type Manifest } from "$fresh/server.ts";
+import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
